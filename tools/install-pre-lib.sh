15e018d6fe1e28c32d20eaf5313256543ff05f58
qianlongxu <qianlongxu@gmail.com>
2023-11-27 15:15:35 +0800

    add ffmpeg workflow

--- tools/install-pre-lib.sh
+++ tools/install-pre-lib.sh
@@ -18,14 +18,11 @@
 set -e
 
 PLAT=$1
-VER=$2
-
-if test -z $PLAT ;then
-    PLAT='all'
-fi
+TAG=$2
 
 THIS_DIR=$(DIRNAME=$(dirname "$0"); cd "$DIRNAME"; pwd)
 cd "$THIS_DIR"
+cd ../
 
 function usage() {
     echo "=== useage ===================="
@@ -33,42 +30,61 @@ function usage() {
     echo "$0 [ios|macos|all] [<release tag>]"
 }
 
+function fix_prefix(){
+    local plat=$1
+    local UNI_PC_DIR="build/product/$plat/universal/$LIB_NAME/lib/pkgconfig"
+    if ls ${UNI_PC_DIR}/*.pc >/dev/null 2>&1;then
+        echo "fix $plat $LIB_NAME pc file prefix"
+        p=$(cd "build/product/$plat/universal/$LIB_NAME";pwd)
+        escaped_p=$(echo $p | sed 's/\//\\\//g')
+        sed -i "" "s/^prefix=.*/prefix=$escaped_p/" "$UNI_PC_DIR/"*.pc
+    fi
+}
+
 function download() {
     local plat=$1
-    echo "===[download $plat $VER]===================="
-    mkdir -p build/pre
-    cd build/pre
-    local fname="$plat-universal-$VER.zip"
-    local url="https://github.com/debugly/MRFFToolChainBuildShell/releases/download/$VER/$fname"
+    local fname="$LIB_NAME-$plat-universal-$VER.zip"
+    local url="https://github.com/debugly/MRFFToolChainBuildShell/releases/download/$TAG/$fname"
+    echo "---[download $fname]-----------------"
     echo "$url"
+    mkdir -p build/pre && cd build/pre
     curl -LO "$url"
     mkdir -p ../product/$plat/universal
     unzip -oq $fname -d ../product/$plat/universal
     if command -v tree >/dev/null 2>&1; then 
         tree -L 2 ../product/$plat/universal
     fi
-    echo "===================================="
+    echo ""
     cd - >/dev/null
 }
 
+function install(){
+    download "$*"
+    fix_prefix "$*"
+}
+
 if [[ "$PLAT" != 'ios' && "$PLAT" != 'macos' && "$PLAT" != 'all' ]]; then
-    echo 'wrong plat,use ios or macos or all!'
+    echo 'plat must use ios or macos'
     usage
     exit
 fi
 
-if test -z $VER ;then
-    #VER=$(git describe --abbrev=0 --tag | awk -F - '{printf "%s-%s",$1,$2}')
+if test -z $TAG ;then
+    echo "tag can't be nil"
     usage
     exit
 fi
+
+# opus-1.3.1-231124151836
+LIB_NAME=$(echo $TAG | awk -F - '{print $1}')
+VER=$(echo $TAG | awk -F - '{print $2}')
 
 if [[ "$PLAT" == 'ios' || "$PLAT" == 'macos' ]]; then
-    download $PLAT
+    install $PLAT
 elif [[ "$PLAT" == 'all' ]]; then
     plats="ios macos"
     for plat in $plats; do
-        download $plat
+        install $plat
     done
 else
     usage
