fcf28fc48dbbd7b02e3f948a1b15d026e4a4a191
qianlongxu <qianlongxu@gmail.com>
2024-02-19 18:43:58 +0800

    update pre install shell

--- install-pre-any.sh
+++ install-pre-any.sh
@@ -30,6 +30,12 @@ DAV1D_TAG='dav1d-1.3.0-231127183948'
 OPENSSL_TAG='openssl-1.1.1w-231127183927'
 DVDREAD_TAG='dvdread-6.1.3-240108102425'
 FFMPEG_TAG='ffmpeg-5.1.4-240123163221'
+
+FREETYPE_TAG='freetype-2.13.2-231229172708'
+UNIBREAK_TAG='unibreak-5.1-231229171455'
+HARFBUZZ_TAG=''
+FRIBIDI_TAG=''
+ASS_TAG=''
 #----------------------------------------------------------
 
 set -e
@@ -55,7 +61,7 @@ function install_lib ()
 function usage() {
     echo "=== useage ===================="
     echo "Download pre-compiled libs from github:"
-    echo " $0 [ios,macos,all] [all|ffmpeg|libyuv|openssl|opus|bluray|dav1d|dvdread]"
+    echo " $0 [ios,macos,all] [all|ffmpeg|libyuv|openssl|opus|bluray|dav1d|freetype|fribidi|harfbuzz|ass]"
     exit 1
 }
 
@@ -100,6 +106,21 @@ if [[ "$PLAT" == 'ios' || "$PLAT" == 'macos' || "$PLAT" == 'all' ]]; then
             ;;
             'dvdread')
                 TAG=$DVDREAD_TAG
+            ;;
+            'freetype')
+                TAG=$FREETYPE_TAG
+            ;;
+            'harfbuzz')
+                TAG=$HARFBUZZ_TAG
+            ;;
+            'fribidi')
+                TAG=$FRIBIDI_TAG
+            ;;
+            'unibreak')
+                TAG=$UNIBREAK_TAG
+            ;;
+            'ass')
+                TAG=$ASS_TAG
             ;;
             *)
                 echo "wrong lib name:$lib"
