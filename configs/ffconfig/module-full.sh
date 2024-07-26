29e96263db90c838c2cfc88d39071d4a95762e4e
qianlongxu <qianlongxu@gmail.com>
2024-07-26 15:39:17 +0800

    enable all filters

--- configs/ffconfig/module-full.sh
+++ configs/ffconfig/module-full.sh
@@ -443,9 +443,7 @@ export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --disable-protocol=unix"
 
 #
 export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --disable-devices"
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --disable-filters"
-
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-filter=nullsink,scale,fps,trim"
+export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-filters"
 
 # External library support:
 export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-iconv"
