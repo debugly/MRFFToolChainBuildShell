45c52f8fc08580a585883130d9ede0ba5f3e39ea
qianlongxu <qianlongxu@gmail.com>
2024-09-26 15:00:22 +0800

    iOS and tvOS default contain libbluray

--- configs/default.sh
+++ configs/default.sh
@@ -1,4 +1,4 @@
 # when '-l all' will use blew default config:
-export ios_default_libs="openssl opus dav1d dvdread freetype fribidi harfbuzz unibreak ass ffmpeg smb2"
-export macos_default_libs="openssl opus bluray dav1d dvdread freetype fribidi harfbuzz unibreak ass ffmpeg smb2"
-export tvos_default_libs="openssl opus dav1d dvdread freetype fribidi harfbuzz unibreak ass ffmpeg smb2"
+export ios_default_libs="openssl opus dav1d dvdread freetype fribidi harfbuzz unibreak ass ffmpeg smb2 bluray"
+export macos_default_libs="openssl opus dav1d dvdread freetype fribidi harfbuzz unibreak ass ffmpeg smb2 bluray"
+export tvos_default_libs="openssl opus dav1d dvdread freetype fribidi harfbuzz unibreak ass ffmpeg smb2 bluray"
