ab511b3f524a9a9d78374e4e41bf5b08aca66316
Anka <runner@Mac-1731645667036.local>
2024-11-15 05:23:07 +0000

    upgrade bluray to bluray-1.3.4-241115132018 by cd

--- configs/libs/bluray.sh
+++ configs/libs/bluray.sh
@@ -30,7 +30,7 @@ export GIT_COMMIT=1.3.4
 export GIT_WITH_SUBMODULE=1
 export REPO_DIR=bluray
 export GIT_REPO_VERSION=1.3.4
-export PRE_COMPILE_TAG=bluray-1.3.4-241014111935
+export PRE_COMPILE_TAG=bluray-1.3.4-241115132018
 
 # you can export GIT_BLURAY_UPSTREAM=git@xx:yy/libbluray.git use your mirror
 if [[ "$GIT_BLURAY_UPSTREAM" != "" ]] ;then
