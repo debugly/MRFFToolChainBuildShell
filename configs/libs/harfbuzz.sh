a1fdf279b4f8ee0a3df1d480d6d61023a18c8426
Anka <runner@Mac-1735280378542.local>
2024-12-27 06:42:09 +0000

    upgrade harfbuzz to harfbuzz-10.1.0-241227143623 by cd

--- configs/libs/harfbuzz.sh
+++ configs/libs/harfbuzz.sh
@@ -29,7 +29,7 @@ export GIT_LOCAL_REPO=extra/harfbuzz
 export GIT_COMMIT=10.1.0
 export REPO_DIR=harfbuzz
 export GIT_REPO_VERSION=10.1.0
-export PRE_COMPILE_TAG=harfbuzz-10.1.0-241219155303
+export PRE_COMPILE_TAG=harfbuzz-10.1.0-241227143623
 
 # you can export GIT_HARFBUZZ_UPSTREAM=git@xx:yy/HARFBUZZ.git use your mirror
 if [[ "$GIT_HARFBUZZ_UPSTREAM" != "" ]] ;then
