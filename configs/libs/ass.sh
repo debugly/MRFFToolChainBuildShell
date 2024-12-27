d08bb749b7980180583f20625361a5cd374cb985
Anka <runner@Mac-1735281415708.local>
2024-12-27 07:06:38 +0000

    upgrade ass to ass-0.17.3-241227150449 by cd

--- configs/libs/ass.sh
+++ configs/libs/ass.sh
@@ -29,7 +29,7 @@ export GIT_LOCAL_REPO=extra/ass
 export GIT_COMMIT=0.17.3
 export REPO_DIR=ass
 export GIT_REPO_VERSION=0.17.3
-export PRE_COMPILE_TAG=ass-0.17.1-241115130816
+export PRE_COMPILE_TAG=ass-0.17.3-241227150449
 
 # you can export GIT_ASS_UPSTREAM=git@xx:yy/ASS.git use your mirror
 if [[ "$GIT_ASS_UPSTREAM" != "" ]] ;then
