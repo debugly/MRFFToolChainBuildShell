dcc74d5d67ae2cbb433181c0c26c2d2f68fa3c88
Anka <runner@Mac-1739326712302.local>
2025-02-12 02:34:29 +0000

    upgrade openssl to openssl-1.1.1w-250212102707 by cd

--- configs/libs/openssl.sh
+++ configs/libs/openssl.sh
@@ -21,7 +21,7 @@ export GIT_LOCAL_REPO=extra/openssl
 export GIT_COMMIT=OpenSSL_1_1_1w
 export REPO_DIR=openssl
 export GIT_REPO_VERSION=1.1.1w
-export PRE_COMPILE_TAG=openssl-1.1.1w-241115131247
+export PRE_COMPILE_TAG=openssl-1.1.1w-250212102707
 
 # you can export GIT_OPUS_UPSTREAM=git@xx:yy/openssl.git use your mirror
 if [[ "$GIT_OPENSSL_UPSTREAM" != "" ]] ;then
