dde11a4113d1ede4f28d7b79efac9cd397de11a7
Anka <runner@Mac-1739414394174.local>
2025-02-13 02:49:36 +0000

    upgrade freetype to freetype-2.13.3-250213104336 by cd

--- configs/libs/freetype.sh
+++ configs/libs/freetype.sh
@@ -27,7 +27,7 @@ export GIT_COMMIT=VER-2-13-3
 export REPO_DIR=freetype
 export GIT_REPO_VERSION=2.13.3
 export GIT_WITH_SUBMODULE=1
-export PRE_COMPILE_TAG=freetype-2.13.3-241227143333
+export PRE_COMPILE_TAG=freetype-2.13.3-250213104336
 
 # you can export GIT_FREETYPE_UPSTREAM=git@xx:yy/FREETYPE.git use your mirror
 if [[ "$GIT_FREETYPE_UPSTREAM" != "" ]] ;then
