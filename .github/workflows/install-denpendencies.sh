5e1a3b68caef002d6267c62a8bc7573e443d3d4a
qianlongxu <qianlongxu@gmail.com>
2025-02-19 11:25:50 +0800

    add apple-android-common workflow

---
+++ .github/workflows/install-denpendencies.sh
@@ -0,0 +1,71 @@
+#!/bin/zsh
+
+set -e
+
+export LIB_NAME=$1
+export PLAT=$2
+
+case LIB_NAME in
+    ass)
+        if [[ $PLAT == all ]];then
+            ./main.sh install -l 'harfbuzz fribidi unibreak' -p ios
+            ./main.sh install -l 'harfbuzz fribidi unibreak' -p tvos
+            ./main.sh install -l 'harfbuzz fribidi unibreak' -p macos
+            ./main.sh install -l 'harfbuzz fribidi unibreak fontconfig' -p android
+        else
+            ./main.sh install -l 'harfbuzz fribidi unibreak' -p $PLAT
+        fi
+    ;;
+    bluray)
+        if [[ $PLAT == android || $PLAT == all ]];then
+            ./main.sh install -p android -l 'fontconfig'
+        fi
+    ;;
+    dav1d)
+    ;;
+    dvdread)
+    ;;
+    ffmpeg)
+        if [[ $PLAT == all ]];then
+            ./main.sh install -l 'openssl opus dav1d dvdread uavs3d smb2 bluray' -p ios
+            ./main.sh install -l 'openssl opus dav1d dvdread uavs3d smb2 bluray' -p tvos
+            ./main.sh install -l 'openssl opus dav1d dvdread uavs3d smb2 bluray' -p macos
+            ./main.sh install -l 'openssl opus dav1d dvdread uavs3d smb2 bluray' -p android
+        else
+            ./main.sh install -l 'openssl opus dav1d dvdread uavs3d smb2 bluray' -p $PLAT
+        fi
+    ;;
+    harfbuzz)
+        if [[ $PLAT == all ]];then
+            ./main.sh install -l 'freetype' -p ios
+            ./main.sh install -l 'freetype' -p tvos
+            ./main.sh install -l 'freetype' -p macos
+            ./main.sh install -l 'freetype' -p android
+        else
+            ./main.sh install -l 'freetype' -p $PLAT
+        fi
+    ;;
+    fontconfig)
+        if [[ $PLAT == android || $PLAT == all ]];then
+            ./main.sh install -p android -l 'freetype'
+        fi
+    ;;
+    freetype)
+    ;;
+    fribidi)
+    ;;
+    openssl)
+    ;;
+    opus)
+    ;;
+    smb2)
+    ;;
+    soundtouch)
+    ;;
+    uavs3d)
+    ;;
+    unibreak)
+    ;;
+    yuv)
+    ;;
+esac
