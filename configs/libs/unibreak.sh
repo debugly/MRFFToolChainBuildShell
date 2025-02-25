8017131b767d2168da94fe87b3f913d8f665057c
Anka <runner@Mac-1740472223792.local>
2025-02-25 08:46:05 +0000

    upgrade unibreak to unibreak-6.1-250225164314 by cd

--- configs/libs/unibreak.sh
+++ configs/libs/unibreak.sh
@@ -29,7 +29,7 @@ export GIT_LOCAL_REPO=extra/unibreak
 export GIT_COMMIT=libunibreak_6_1
 export REPO_DIR=unibreak
 export GIT_REPO_VERSION=6.1
-export PRE_COMPILE_TAG=unibreak-6.1-250224130239
+export PRE_COMPILE_TAG=unibreak-6.1-250225164314
 
 # you can export GIT_UNIBREAK_UPSTREAM=git@xx:yy/UNIBREAK.git use your mirror
 if [[ "$GIT_UNIBREAK_UPSTREAM" != "" ]] ;then
