44354d3af0e15ac36398de25245032f55c4d250e
Anka <runner@Mac-1740562832676.local>
2025-02-26 09:49:03 +0000

    upgrade freetype to freetype-2.13.3-250226174825 for ios by cd

--- configs/libs/freetype.sh
+++ configs/libs/freetype.sh
@@ -37,4 +37,5 @@ fi
 
 # pre compiled
 export PRE_COMPILE_TAG=freetype-2.13.3-250225223932
+export PRE_COMPILE_TAG_IOS=freetype-2.13.3-250226174825
 export PRE_COMPILE_TAG_ANDROID=freetype-2.13.3-250226152149
